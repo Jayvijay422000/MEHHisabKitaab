b6d86e07b84f20b1766ebd187deb5aecc2f0b73b
mukeshKumbhar <prajapatmukesh316@gmail.com>
2023-10-05 16:50:27 +0530

    students

--- modules/students/updateStud.js
+++ modules/students/updateStud.js
@@ -5,8 +5,8 @@ const updateStud= async(req,res)=>{
         try {
            
             const updateUser = await studmodel.findOneAndUpdate(
-                { mobile_number:req.body['mobile_number'],_id:req.body['studId'],active:true},
-                { $set:req.body, updated_by:req.userId}, // Dynamically set all fields from req.body
+                { mobile_number:req.body['mobile_number'],_id:req.body['studId'],active:true,updated_by:req.userId},
+                { $set:req.body}, // Dynamically set all fields from req.body
                 { new: true }
               );
               if(!updateUser){
